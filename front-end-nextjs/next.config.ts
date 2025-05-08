ebdc93e388db9cb35fc19f7e3da8961ad4dd0001
haks7 <sumana.pinjarla@gmail.com>
2025-05-08 16:32:29 +1000

    Modified GitHub Actions workflow for Docker

--- front-end-nextjs/next.config.ts
+++ front-end-nextjs/next.config.ts
@@ -1,16 +1,3 @@
-const BACKEND_URL = process.env.BACKEND_URL;
-
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/vehicle-optimization",
-        destination: `${BACKEND_URL}/api/vehicle-optimization`,
-      },
-      {
-        source: "/api/vehicle-security",
-        destination: `${BACKEND_URL}/api/vehicle-security`,
-      },
-    ];
-  },
+  distDir: 'out', // Directs the build output to the 'out' directory
 };
